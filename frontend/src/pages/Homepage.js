a1990544c172bf02fa44955713ecdd7d6283d275
jeetendrakumar01 <jeetendrakumar5861@gmail.com>
2025-03-11 16:04:16 +0530

    commit

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -22,7 +22,7 @@ const Homepage = () => {
                             System
                         </StyledTitle>
                         <StyledText>
-                            Streamline school management, class organization, and add students and faculty.
+                            Streamline University management, class organization, and add students and faculty.
                             Seamlessly track attendance, assess performance, and provide feedback.
                             Access records, view marks, and communicate effortlessly.
                         </StyledText>
